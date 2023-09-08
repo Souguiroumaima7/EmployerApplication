c8026399a548d1dc20ab5d1910b9f339ecc84b66
Oumaima Souguir <81293137+Souguiroumaima7@users.noreply.github.com>
2023-09-08 11:37:34 +0100

    Buid Compoments in src directory

---
+++ EmployerAppFrontend/employerapp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<NavbarCompoment/>
+<EmployeeCompoments/>
+<DepartementCompoments/> 
+<ProjetCompoments/>
+<SpaceParkingCompoment/>
+</template>
+
+<script>
+import NavbarCompoment from './components/NavbarCompoment.vue';
+import DepartementCompoments from './components/DepartementCompoments.vue';
+import EmployeeCompoments from './components/EmployeeCompoments.vue';
+import ProjetCompoments from './components/ProjetCompoment.vue';
+import SpaceParkingCompoment from './components/SpaceParkingCompoment.vue';
+export default {
+  name: 'App',
+  components: {
+    NavbarCompoment,
+    DepartementCompoments,
+    EmployeeCompoments,
+    ProjetCompoments,
+    SpaceParkingCompoment
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
